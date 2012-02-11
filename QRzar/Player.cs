4294991ca07fa670224348729dae2a2a475e4db3
Eoin Flanagan <flanagep@tcd.ie>
2012-02-11 13:13:52 +0000

    Separated the Player class from the Game class file

---
+++ QRzar/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Runtime.Serialization;
+
+namespace QRzar
+{
+    [DataContract]
+    public class Player
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public String name { get; set; }
+        [DataMember]
+        public int kills { get; set; }
+
+    }
+
+}
+
